81a51dd7776cfca29f37d94e3a029371e00b67ac
Daryll Joshua Magsombol <darylljoshuamagsombol@Darylls-MacBook-Air.local>
2025-07-16 11:26:06 +0800

    removed lg in our journey section

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -266,17 +266,17 @@ const MainHome = () => {
                   src={prenupiamges[currentImageIndex]}
                   alt="Slideshow"
                   className="w-full h-full object-cover"
-                  initial={{ opacity: 0.5, x: -100 }}
+                  initial={{ opacity: 0, x: -100 }}
                   animate={{ opacity: 1, x: 0 }}
                   exit={{ opacity: 0, scale: 0.9 }}
-                  transition={{ duration: 1 }}
+                  transition={{ duration: 1, ease: 'easeInOut' }}
                 />
               </div>
             </motion.div>
 
             {/* Content overlay on the right */}
             <motion.div
-              className="w-screen h-[50vh] md:absolute md:top-50 md:right-20 md:w-[45vw] md:h-[70vh] lg:h-[100vh] flex flex-col items-center md:items-start justify-center text-center md:text-left p-6 sm:p-12 bg-white bg-opacity-90"
+              className="w-screen h-[50vh] md:absolute md:top-50 md:right-20 md:w-[45vw] md:h-[70vh] flex flex-col items-center md:items-start justify-center text-center md:text-left p-6 sm:p-12 bg-white bg-opacity-90"
               initial={{ opacity: 0, x: 50 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ delay: 0.5, duration: 1.3, ease: 'easeInOut' }}
