4d20182b9ce3d1fd4b5b9db9c39c61ff253cc296
Daryll Joshua Magsombol <darylljoshuamagsombol@Darylls-MacBook-Air.local>
2025-07-28 11:40:04 +0800

    major change: family per rsvp, add table number, find seat

--- database/migrations/1753637359294_create_family_invitation_guests_table.ts
+++ database/migrations/1753637359294_create_family_invitation_guests_table.ts
@@ -1,21 +1,20 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'invitation_keys'
+  protected tableName = 'family_invitation_guests'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('code').notNullable()
+      table.string('name').notNullable()
       table
-        .integer('invitation_guest_id')
+        .integer('family_invitation_id')
         .unsigned()
-        .references('invitation_guests.id')
-        .notNullable()
-      table.timestamp('valid_until').notNullable()
-      table.string('created_by')
+        .references('id')
+        .inTable('family_invitations')
+        .onDelete('CASCADE')
+      table.string('table_number').nullable()
       table.timestamp('created_at')
-      table.string('updated_by')
       table.timestamp('updated_at')
     })
   }
